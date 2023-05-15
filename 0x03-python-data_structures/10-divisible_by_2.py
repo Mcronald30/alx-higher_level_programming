3e7780ab66901ddab28513b42318c47c6743c209
Ronald <nottinsonstephen@gmail.com>
2023-05-15 13:34:25 -0700

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
     result = []
     for i in my_list:
         if i % 2:
-            result = result + [True]
-        else:
             result = result + [False]
+        else:
+            result = result + [True]
     return result
