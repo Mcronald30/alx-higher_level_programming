cdb83584821a0a0000be4a57557a5c5a96ef5e00
Ronald <nottinsonstephen@gmail.com>
2023-05-30 08:58:30 -0700

    an empty class called Square that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-class Square:
-    """
-    A class that defines a square.
+"""A class that defines a square."""
 
-    This class serves as a blueprint for creating square objects.
-    """
+class Square:
+
+
+    """This class serves as a blueprint for creating square objects."""
+
     pass
