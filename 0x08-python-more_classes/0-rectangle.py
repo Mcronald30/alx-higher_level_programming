ad90470ec0a2eceee61a6fbf63a039da0040f9a5
Ronald <nottinsonstephen@gmail.com>
2023-06-06 06:17:58 -0700

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Creating a Class  Rectangle
-"""
+"""Creating a Class  Rectangle"""
+
 class Rectangle:
-    """Define class Rectangle
-    """
+    """Define class Rectangle"""
     pass
