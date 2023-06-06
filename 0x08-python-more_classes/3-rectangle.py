80453738ded8876de36525ddeb9a6562399be49b
Ronald <nottinsonstephen@gmail.com>
2023-06-06 07:02:03 -0700

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-""" Creating class of a rectangle """
+"""Creating Class Rectangle"""
 
 
 class Rectangle:
+    """Define the class rectangle."""
+
     def __init__(self, width=0, height=0):
-        """intialize the class rectangle
+        """Initialize the Rectangle.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get the width of the rectangle"""
+        """Get the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if not type(int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -24,28 +26,27 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get the height of the rectangle"""
+        """Get the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if not type(int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        return self.__width * self.__height
+        """Calculating area of the rectangle."""
+        return self.width * self.height
 
     def perimeter(self):
-        """Calculating  perimeter of the rectangle"""
-        if self.width == 0 or self.height == 0:
-            return 0
-            return (self.width + self.height) * 2
+        """Calculating perimeter of the rectangle."""
+        return 2 * (self.width + self.height)
 
     def __str__(self):
-        """Print Rectangle."""
+        """Printing rectangle."""
         if self.width == 0 or self.height == 0:
             return ""
         return ((("#" * self.width) + "\n") * self.height)[:-1]
