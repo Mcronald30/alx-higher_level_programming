b312c584163cf39435a69a1fad00221fcd0b127b
Ronald <nottinsonstephen@gmail.com>
2023-06-14 07:19:06 -0700

    a function that returns the dictionary description with simple data structure

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -10,15 +10,4 @@ def class_to_json(obj):
     with simple data structure (list, dictionary, string,
     integer and boolean) for JSON serialization of an object
     """
-    description = {}
-    description['__class__'] = obj.__class__.__name__
-    description['__attributes__'] = {}
-
-
-    """Iterate over object attributes"""
-    for attr, value in obj.__dict__.items():
-        """Serialize simple data types
-        (list, dictionary, string, integer, boolean)"""
-        if isinstance(value, (list, dict, str, int, bool)):
-            description['__attributes__'][attr] = value
-            return description
+    return obj.__dict__
