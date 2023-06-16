3beec4d8727ed3641ba4043735fef3c931d26f2c
Ronald <nottinsonstephen@gmail.com>
2023-06-16 12:02:57 -0700

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 Class Base Module
 """
 import json
+import csv
+import turtle
 
 
 class Base:
@@ -68,3 +70,98 @@ class Base:
                 return instances
         except FileNotFoundError:
             return []
+
+    @staticmethod
+    def to_csv_row(obj):
+        """adding the class methods"""
+        if isinstance(obj, Rectangle):
+            return [obj.id, obj.width, obj.height, obj.x, obj.y]
+        elif isinstance(obj, Square):
+            return [obj.id, obj.size, obj.x, obj.y]
+
+    @staticmethod
+    def from_csv_row(row):
+        """Rectangle class method"""
+        if len(row) == 5:
+            return Rectangle(int(row[0]), int(row[1]), int(row[2]), int(row[3]), int(row[4]))
+        elif len(row) == 4:
+            return Square(int(row[0]), int(row[1]), int(row[2]), int(row[3]))
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """claas method to save file"""
+        filename = cls.__name__ + ".csv"
+        rows = [cls.to_csv_row(obj) for obj in list_objs]
+
+        with open(filename, "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerows(rows)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """class method to load file"""
+        filename = cls.__name__ + ".csv"
+        objs = []
+
+        with open(filename, "r") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                obj = cls.from_csv_row(row)
+                objs.append(obj)
+        return objs
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """creating draw class"""
+        turtle.setup(800, 600)
+        window = turtle.Screen()
+        window.bgcolor("white")
+        t = turtle.Turtle()
+        t.speed(2)
+
+        for rectangle in list_rectangles:
+            t.penup()
+            t.goto(rectangle.x, rectangle.y)
+            t.pendown()
+            t.forward(rectangle.width)
+            t.left(90)
+            t.forward(rectangle.height)
+            t.left(90)
+            t.forward(rectangle.width)
+            t.left(90)
+            t.forward(rectangle.height)
+            t.left(90)
+
+        for square in list_squares:
+            t.penup()
+            t.goto(square.x, square.y)
+            t.pendown()
+            t.forward(square.size)
+            t.left(90)
+            t.forward(square.size)
+            t.left(90)
+            t.forward(square.size)
+            t.left(90)
+            t.forward(square.size)
+            t.left(90)
+
+        turtle.done()
+
+class Rectangle(Base):
+    """Rectangle base class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initializing the rect. base"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+class Square(Base):
+    """Square base class module"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializing the square"""
+        super().__init__(id)
+        self.size = size
+        self.x = x
+        self.y = y
