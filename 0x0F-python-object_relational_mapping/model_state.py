f258b3af635d9d58cc45089af2e8ac6694e6e3dc
Ronald <nottinsonstephen@gmail.com>
2023-08-21 10:49:37 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Create the states table using SQLAlchemy"""
-import sys
-from sqlalchemy import create_engine
-from model_state import Base, State
+"""Define a State class that
+links to the MySQL table states"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
